7e9ca8a4a9004c3110c6af724ea26f4e8533218f
Domenica Katherine Amores Cedeño <domenicaamores2006@gmail.com>
2026-07-19 19:35:07 -0500

    feat(app): implement grid structure

---
+++ weather-dashboard/src/App.tsx
@@ -0,0 +1,37 @@
+import { Grid } from '@mui/material';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center"}}>
+
+         {/* Encabezado */}
+         <Grid size={{xs: 12, md: 12}}>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid size={{xs: 12, md: 12}}>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid size={{xs: 12, md: 3}}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{xs: 12, md: 9}}>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid sx={{display: {xs: "none", md: "block"}}}>Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid sx={{display: {xs: "none", md: "block"}}}>Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid size={{xs: 12, md: 12}}>Elemento: Información adicional</Grid>
+
+      </Grid>
+    </>
+  )
+}
+
+export default App
